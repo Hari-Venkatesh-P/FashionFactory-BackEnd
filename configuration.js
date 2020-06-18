7689ec85d7c6b7fa885091bfa1f0006fc2b5f0c3
HariVenkateshP <harivenkatesh26299@gmail.com>
2020-06-18 18:07:23 +0530

    API Dev Completed

--- configuration.js
+++ configuration.js
@@ -7,11 +7,7 @@ const dbdetails = {
 }
 
 const aws = {
-	accesskey: 'AKIAZ2OYNEWLQGA256GG',
-	secretkey: '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',
-	region : 'ap-south-1',
-    bucket: 'hari-bucket-s3',
-    productimages : 'Product',
+	
 };
 
 
