856f3f31b17e5221c0970cf7c1357fa13c819226
Hari Venkatesh P <harivenkat1110@gmail.com>
2020-06-21 02:01:35 +0530

    Removed AWS Creds

--- configuration.js
+++ configuration.js
@@ -6,13 +6,13 @@ const dbdetails = {
     port : "33281",
 }
 
-const aws = {
-    accesskey: 'AKIAZ2OYNEWLQGA256GG',
-	secretkey: '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',
-	region : 'ap-south-1',
-    bucket: 'hari-bucket-s3',
-    productimages : 'Product',
-};
+// const aws = {
+//     accesskey: 'AKIAZ2OYNEWLQGA256GG',
+// 	secretkey: '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',
+// 	region : 'ap-south-1',
+//     bucket: 'hari-bucket-s3',
+//     productimages : 'Product',
+// };
 
 
 
